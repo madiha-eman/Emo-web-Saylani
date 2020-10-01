2ef361a883d7c2f906e423118ee62af16bdcf888
musammarabbas123 <musammarabbas123@gmail.com>
2020-10-01 09:20:54 +0500

    second folder

---
+++ js/index.js
@@ -0,0 +1,46 @@
+// var name = "Madiha"
+// var myClass = "4thyear"
+
+// console.log(`my name is ${name} 
+// i am student of ${myClass} `)
+// var year = prompt("Enter yiur birth year")
+// Number = year
+// var age = 2020 +year;
+// console.log(`my age is ${age}`);
+// var name = prompt("Enter your name")
+// var age= prompt("Enter your age?")
+// if (age > 25){
+//     console.log(`${name} Enter in your class room `)
+// }
+// else if( age <= 25)
+// console.log(` ${name} you are not allowed`)
+
+// console.log("Have a nice day!")
+
+// var age = prompt("Enter your age")
+// var permit = prompt("Do you have parents permission")
+// var NIC = prompt("Do you have NIC")
+// if(age > 15 && permit == "yes"){
+//  console.log("you are Allowed")
+// }
+// else if(age > 15 && NIC == "yes" )
+// console.log("you are allowed")
+
+// else
+// console.log("you are not allowed")
+
+var friends = ["Asma", "Maryam", "Khizra", "Faiza"]
+
+
+//  console.log(friends[4]);
+// console.log(friends[3])
+// friends.unshift("rumaisa","eman")
+// friends.pop(1)
+friends.push("tania")
+console.log(friends)
+
+
+
+
+
+
