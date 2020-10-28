18bec4b0d61a2f6610e0745e4e245347e27a5460
madiha-name <musammarabbas123@gmail.com>
2020-10-28 13:12:25 +0500

    addition

---
+++ js/java/index.js
@@ -0,0 +1,96 @@
+// 1
+// alert("Madiha \n 4thyear")
+//alert(`madiha
+//4thyear`)
+
+//2
+// var birthYear = "birth year 1997"
+// console.log(`type of birthyear is =` + typeof birthYear)
+
+//3
+// document.write("Rules for naming JS variables");
+
+
+
+
+// 4
+// var age = +prompt("write your age?" )
+// var days = age * 365;
+// console.log(days + "days")
+
+//5
+// var num = prompt("write a number?")
+// var input1 = ++num;
+// alert(input1)
+
+//OR
+
+// with function
+// var addNumber;
+// function inc(num){
+// addNum = ++num;
+// return addNum
+// }
+// var newAddition = inc(5);
+// alert(newAddition)
+
+
+//6
+// var num = prompt("write a number?")
+// var input1 = --num;
+// alert (input1)
+
+//OR
+
+// with function
+// var subNumber;
+// function inc(num){
+// subNum = --num;
+// return subNum
+// }
+// var newsubtraction = inc(5);
+// alert(newsubtraction )
+
+//7 
+// var num = prompt("write a square number") 
+// var square = num * num;
+// alert(square)
+
+// OR
+
+//  function with
+// var num3 ;
+// function square(num){
+//     num3 = num * num   
+// }
+// square(4)
+//     alert(num3)
+
+//8
+// var num = prompt("number")
+// if (num > 0)
+// {
+// console.log ("positive integer")
+// }
+
+// else if (num < 0){
+// console.log("negative integer")
+// }
+
+
+//9
+// var currancy = prompt("write currency here?")
+
+
+
+// 10
+// var cityName = prompt("Enter your city name")
+// if (cityName === "karachi"){
+//     alert("Welcome to city of lights");
+// }
+// else if (cityName === "lahore"){
+//     alert("welcome to historical city");
+// }
+// else if (cityName === "quetta"){
+//     alert("welcome to cold city");
+// }
