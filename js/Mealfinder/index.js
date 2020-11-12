a5fb087a06374771316cb53e1aa36a9696f67396
madiha-eman <72120359+madiha-eman@users.noreply.github.com>
2020-11-12 13:05:48 +0500

    git add .
    done

--- js/Mealfinder/index.js
+++ js/Mealfinder/index.js
@@ -1,5 +1,8 @@
 
-function searchRecipe(){
+// var submit  = document.getElementById('sub');
+// console.log(submit)
+
+function searchRecipe() {
     var search  =  document.getElementById('search');
     console.log(search.value) 
     var api2 = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search.value}`
@@ -11,27 +14,25 @@ function searchRecipe(){
         // Your code for handling the data you get from the API
         console.log(data.meals)
         const recipesArr = data.meals;
-    
         var row  =  document.getElementById('row');
-        ;
-    
         for(let i = 0; i< recipesArr.length; i++){
-        var div = document.createElement('div')
+        var div1 = document.createElement('div')
+        var div2 = document.createElement('div')
         var img = document.createElement('img')
-        div.className = 'box';
-        img.className = 'img'
-
+        div1.className = 'box';
+        img.className = 'img1'
+        
+        // text.className = 'text2'
         img.setAttribute("src", recipesArr[i].strMealThumb);
-        div.appendChild(img);
-
+        
         var text = document.createTextNode(recipesArr[i].strMeal)
-        div.appendChild(text)
-        row.appendChild(div)
+        // div.appendChild(div)
+        div2.appendChild(img);
+        div2.appendChild(text)
+        div1.appendChild(div2)
+        row.appendChild(div1)
     
         }
-    
-    
-    
     
     })
     .catch(function(error) {
@@ -42,21 +43,35 @@ function searchRecipe(){
     }
     
 
-    function searchRandom(){
+    function searchRandom() {
         var api = 'https://www.themealdb.com/api/json/v1/1/random.php'
-         fetch(api)
-         .then(res =>res.json))
-         .then(data => function(){
-             console.log(data)
-             var recipeSingle = data.meals
-             for(let i = 0; i< recipesArr.length; i++){
-                var row = document.getElementById('row')
-                var div = document.createElement('div')
+        fetch(api)
+        .then(res=>res.json())
+        .then(data => {
+            console.log(data)
+            var recipeSingle= data.meals
+            for(let i = 0; i< recipeSingle.length; i++){
+                var row  =  document.getElementById('row');
+                var div1 = document.createElement('div')
+                var div2 = document.createElement('div')
                 var content = document.createElement('div')
-                var textcontent = document.createTextNode(recipeSingle[i].strMeal)
-
-         }
-            
-
-    })
-         
+                var img = document.createElement('img')
+                var textcontent = document.createTextNode(recipeSingle[i].strInstructions)
+                content.appendChild(textcontent)
+                var text = document.createTextNode(recipeSingle[i].strMeal)
+                // textcontent.className = 'head'
+                img.className = 'imgRan'
+                div1.className = 'main'
+                content.className = 'para'
+                img.setAttribute("src", recipeSingle[i].strMealThumb);
+                div1.appendChild(text)
+                row.appendChild(div1)
+                div2.appendChild(img);
+                div1.appendChild(div2)
+                 row.appendChild(content)
+            }
+      
+        });
+      
+    }
+    submitForm.addEventListener('submit', searchRecipe);
